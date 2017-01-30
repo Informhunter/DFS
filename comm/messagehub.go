4f0962e9d9cfa334371221e6e90a53566d257238
Informhunter <informhunter@gmail.com>
2017-01-30 16:56:27 +0300

    Great job. No new connections are created for every message. Top notch response time improvement.

--- comm/messagehub.go
+++ comm/messagehub.go
@@ -13,9 +13,11 @@ type MessageHandler interface {
 type MessageHub struct {
 	nodeManager     *node.NodeManager
 	messageHandlers map[MessageType][]MessageHandler
+	outConnMap      map[string]net.Conn
 }
 
 func (msgHub *MessageHub) Listen(nodeManager *node.NodeManager, addr string) error {
+	msgHub.outConnMap = make(map[string]net.Conn, 0)
 	msgHub.nodeManager = nodeManager
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
@@ -27,12 +29,19 @@ func (msgHub *MessageHub) Listen(nodeManager *node.NodeManager, addr string) err
 			if err != nil {
 				continue
 			}
-			msg := new(Message)
-			dec := gob.NewDecoder(conn)
-			dec.Decode(msg)
-			for _, msgHandler := range msgHub.messageHandlers[msg.Type] {
-				msgHandler.HandleMessage(msg)
-			}
+			go func() {
+				for {
+					msg := new(Message)
+					dec := gob.NewDecoder(conn)
+					err := dec.Decode(msg)
+					if err != nil {
+						break
+					}
+					for _, msgHandler := range msgHub.messageHandlers[msg.Type] {
+						msgHandler.HandleMessage(msg)
+					}
+				}
+			}()
 		}
 	}()
 	return nil
@@ -47,29 +56,26 @@ func (msgHub *MessageHub) Subscribe(msgHandler MessageHandler, msgTypes ...Messa
 	}
 }
 
-func (msgHub *MessageHub) Send(msg Message, nodeName string) error {
+func (msgHub *MessageHub) Send(msg Message, nodeName string) (err error) {
 	msg.SourceNode = msgHub.nodeManager.This.Name
 	node := msgHub.nodeManager.Node(nodeName)
-	conn, err := net.Dial("tcp", node.PrivateAddress)
-	if err != nil {
-		return err
+	conn, exists := msgHub.outConnMap[node.PrivateAddress]
+	if !exists {
+		conn, err = net.Dial("tcp", node.PrivateAddress)
+		if err != nil {
+			return err
+		}
+		msgHub.outConnMap[node.PrivateAddress] = conn
 	}
 	enc := gob.NewEncoder(conn)
 	enc.Encode(msg)
-	conn.Close()
 	return nil
 }
 
 func (msgHub *MessageHub) Broadcast(msg Message) error {
 	msg.SourceNode = msgHub.nodeManager.This.Name
 	for _, node := range msgHub.nodeManager.Nodes() {
-		conn, err := net.Dial("tcp", node.PrivateAddress)
-		if err != nil {
-			return err
-		}
-		enc := gob.NewEncoder(conn)
-		enc.Encode(msg)
-		conn.Close()
+		msgHub.Send(msg, node.Name)
 	}
 	return nil
 }
